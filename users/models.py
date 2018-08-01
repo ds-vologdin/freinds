10a24b748a5e19513ee7c6de4232c2e8182f3433
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-08-01 14:48:42 +0300

    Модель User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models import CharField, DateField, SlugField
+from django.db.models import ManyToManyField
 
-# Create your models here.
+
+# Примеры заполнения моделей смотрите в модулях factories и management.commands
+class User(models.Model):
+    username = SlugField(max_length=50, unique=True)
+    name = CharField(max_length=200)
+    birthday = DateField(null=True, blank=True)
+    freinds = ManyToManyField('self')
+
+    def __str__(self):
+        return '{}: {}'.format(self.code_name, self.name)
