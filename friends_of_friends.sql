60cdf8b00bb92889ae9d82a15a78b7114a4c1b1d
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-08-01 21:03:46 +0300

    raw-sql друзья друзей

---
+++ friends_of_friends.sql
@@ -0,0 +1,28 @@
+CREATE TABLE "users" (
+  "id" serial NOT NULL PRIMARY KEY,
+  "username" varchar(50) NOT NULL UNIQUE,
+  "name" varchar(200) NOT NULL,
+  "birthday" date NULL
+);
+
+CREATE TABLE "friends" (
+  "id" serial NOT NULL PRIMARY KEY,
+  "from_user_id" integer NOT NULL,
+  "to_user_id" integer NOT NULL,
+  FOREIGN KEY ("from_user_id") REFERENCES "users" ("id") ON DELETE CASCADE,
+  FOREIGN KEY ("to_user_id") REFERENCES "users" ("id") ON DELETE CASCADE
+);
+
+
+CREATE INDEX friends_from_user_id_indx ON friends (from_user_id);
+CREATE INDEX friends_to_user_id_indx ON friends (to_user_id);
+
+WITH RECURSIVE r AS (
+    SELECT from_user_id, to_user_id
+	FROM friends
+	WHERE from_user_id=3
+    UNION
+    SELECT friends.from_user_id, friends.to_user_id
+	FROM friends JOIN r ON r.from_user_id = friends.to_user_id
+)
+SELECT from_user_id, to_user_id FROM r ORDER BY from_user_id;
